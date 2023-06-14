51bd27c6f0b48ceb62e6ca07e735ceebc98147c1
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-06-14 15:15:14 -0300

    refactor: add validations to test2

--- fakeData.js
+++ fakeData.js
@@ -4,21 +4,6 @@ const fakeData  =  [
         id: 1,
         name: "João Oliveira",
         job: "Desenvolvedor"
-    },
-    {
-        id: 2,
-        name: "Julio",
-        job: "Desenvolvedor"
-    },
-    {
-        id: 3,
-        name: "Mario",
-        job: "Desenvolvedor"
-    },
-    {
-        id: 4,
-        name: "Glauber",
-        job: "Desenvolvedor"
     }
 ]
 
--- teste2.js
+++ teste2.js
@@ -3,16 +3,20 @@ const data =  require("./fakeData");
 module.exports = function(req, res){
   const { name, job } =  req.body;
 
-	const lastItem = data[data.length -1];
-    
+	if (!name || !job) {
+		return res.status(400).send("Necessário fornecer nome e profissão do usuário");
+	}
+
+	const user = data.find((user) => user.name === name);
+  if (user) return res.status(409).send("Usuário já cadastrado");
+
 	const newUser = {
-		id: lastItem.id + 1,
+		id: data.length + 1,
 		name,
 		job,
 	}
 
 	data.push(newUser)
 
-	res.status(201).send(newUser);
-
+	return res.status(201).send(newUser);
 };
